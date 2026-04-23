fca811e5ad8373792ffd6f34c9b1f8118b26bcb0
Mohammad Danish <mdtabish790331@gmail.com>
2026-04-24 03:30:42 +0530

    Fixed data_loader for Render DB

--- data_loader.py
+++ data_loader.py
@@ -1,19 +1,26 @@
 import psycopg2
 import pandas as pd
-import psycopg2
-import pandas as pd
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
+
+def get_connection():
+    database_url = os.getenv("DATABASE_URL")
+    if database_url:
+        conn = psycopg2.connect(database_url)
+    else:
+        conn = psycopg2.connect(
+            host=os.getenv("DB_HOST"),
+            port=os.getenv("DB_PORT"),
+            dbname=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD")
+        )
+    return conn
+
 def load_data(stock):
-    conn = psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        dbname=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD")
-    )
+    conn = get_connection()
 
     query = f"""
     SELECT datetime, open, high, low, close, volume
